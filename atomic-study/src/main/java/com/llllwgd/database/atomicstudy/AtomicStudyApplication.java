9a31c569dd91b70cfcb409c7b4e9bfb66546d4f6
llllwgd <guodong.wang@doubao.onaliyun.com>
2020-06-26 07:28:29 +0800

    init

---
+++ atomic-study/src/main/java/com/llllwgd/database/atomicstudy/AtomicStudyApplication.java
@@ -0,0 +1,13 @@
+package com.llllwgd.database.atomicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicStudyApplication.class, args);
+	}
+
+}
---
+++ atomic-study/src/test/java/com/llllwgd/database/atomicstudy/AtomicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llllwgd.database.atomicstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
